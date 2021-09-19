93affafef1444a5d0b9d5205f249e06f7d783b99
koresachin7 <koresachin7@gmail.com>
2021-09-20 02:25:46 +0530

    Added : Ability to manage Employee Wage Of Muliple Companies Using Interface Approach

---
+++ src/com/bridgelabz/ComputeWage.java
@@ -0,0 +1,6 @@
+package com.bridgelabz;
+
+public interface ComputeWage {
+    public void addCompanyEmpWage(String company,int empRatePerHour,int numOfWorkingDays,int maximumHoursPerMonth);
+    public void computeEmpWage();
+}
